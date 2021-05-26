31a3f01d657268915f6efb99e31b7ca6dc1fed77
Crowdbotics <team@crowdbotics.com>
2021-05-26 20:34:17 +0000

    Update models

--- backend/free_to_feed_27217/wsgi.py
+++ backend/free_to_feed_27217/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'free_to_feed_27217.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "free_to_feed_27217.settings")
 
 application = get_wsgi_application()
